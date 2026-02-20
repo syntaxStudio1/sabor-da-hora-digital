c9915dba29a31bccb9942afbcec93d52a82a2d88
Éder Melero Júnior <143884150+EderMelero@users.noreply.github.com>
2026-02-19 22:28:31 -0300

    Solucao definitiva tela branca

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Se você usa rotas
-import App from './App'
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* O basename deve ser o nome exato do seu repositório no GitHub */}
-    <BrowserRouter basename="/Sabor-dahora">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // Verifique se está 'plugin-react'
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Sabor-dahora/", // Nome exato do seu repositório
+  // O ponto antes da barra obriga o navegador a procurar os arquivos 
+  // na pasta atual, não importa qual seja o nome do repositório.
+  base: "./", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    // Garante que os arquivos gerados tenham nomes simples
+    outDir: "dist",
+  }
 });
